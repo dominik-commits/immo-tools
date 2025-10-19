572af56c449d982e26d6c89243fbc59e638872af
Dominik Kümmel <dominik@theleanteam.de>
2025-10-19 20:01:28 +0200

    feat(api): CJS ping + minimal rewrites

---
+++ api/ping.cjs
@@ -0,0 +1,9 @@
+﻿module.exports = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).end(JSON.stringify({
+    ok: true,
+    ts: Date.now(),
+    url: req.url,
+    message: "Serverless Function is alive ✅"
+  }));
+};
