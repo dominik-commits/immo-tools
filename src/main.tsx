8a1e123573e8d6d764d6435cd9230b55095ea5ce
Dominik Kümmel <dominik@theleanteam.de>
2025-10-21 09:31:25 +0200

    style: apply Propora theme globally

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import './index.css';                 // Tailwind (base/components/utilities)
+import './styles/propora-theme.css';  // <- NACH index.css importieren
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
